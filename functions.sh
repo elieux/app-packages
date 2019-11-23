8158a4273ebb2bf539690501e6a869d100a7170b
David Macek <david.macek.0@gmail.com>
2019-11-23 17:37:48 +0100

    Add function _pickarch()
    It sorts out files extracted and auto-renamed by 7z that differ only
    by architecture.

--- functions.sh
+++ functions.sh
@@ -57,3 +57,16 @@ _rmmsdll() {
         "api-ms-win-[0-9a-z-]+")
     find "${1}" -iname '*.dll' | grep -Eif <(for m in "${ms[@]}"; do echo "/${m}[.]dll$"; done) | xargs -r rm
 }
+
+_pickarch() {
+    local f n
+
+    for f in "${@}"
+    do
+        n="$(echo "${f}" | sed -r 's/_[0-9]+[.]/./')"
+        case "$(file "${f}")" in
+        *x86-64*|*assembly*) mv -f "${f}" "${n}" ;;
+        *) rm "${f}" ;;
+        esac
+    done
+}
